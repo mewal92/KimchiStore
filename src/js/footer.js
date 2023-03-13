2fa166159bff7be7c8f56ae7960ec0c63509180f
Lamers <gustav@henrikssons.org>
2023-03-13 20:59:13 +0100

    Removed unused endtag </head>

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,6 +1,5 @@
 function footer(){
     const footer = document.querySelector("#footer");
-    
     footer.innerHTML = `
     <b>Copyright &copy; 2023 - Melinda Walter, Oscar Jidåker, Kevin Dybeck, Gustav Henriksson</b>
     `
