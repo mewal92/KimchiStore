57b1e1e1bebf0a8be15482aa6404f2905d03e45c
Lamers <gustav@henrikssons.org>
2023-03-13 20:37:09 +0100

    Fixed java script for footer

---
+++ src/js/footer.js
@@ -0,0 +1,8 @@
+function footer(){
+    const footer = document.querySelector("#footer");
+    
+    footer.innerHTML = `
+    <b>Copyright &copy; 2023 - Melinda Walter, Oscar Jidåker, Kevin Dybeck, Gustav Henriksson</b>
+    `
+}
+footer();
