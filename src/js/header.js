dafb01f5a7fe091eeb3827f3119de5cf55d0a5e0
Lamers <gustav@henrikssons.org>
2023-03-14 00:07:55 +0100

    Changed back tags

--- src/js/header.js
+++ src/js/header.js
@@ -8,34 +8,14 @@ function header(){
     header.innerHTML = `
     <nav class="nav">
         <ul>
-          <li>
-            <a
-              class="transition-all duration-200 hover-color-dark-gray:hover ${window.document.location.pathname === "/kontakt.html" ? "text-black" : null }"
-              href="kontakt.html"
-              >Kontakt</a
-            >
-          </li>
-          <li>
-            <a
-              class="transition-all duration-200 hover-color-dark-gray:hover ${window.document.location.pathname === "/produkter.html" ? "text-black" : null }"
-              href="produkter.html"
-              >Produkter</a
-            >
-          </li>
-          <li>
-            <a
-              class="transition-all duration-200 hover-color-dark-gray:hover ${window.document.location.pathname === "/order.html" ? "text-black" : null }"
-              href="order.html"
-              >Beställning</a
-            >
-          </li>
-          <li>
-            <a
-              class="transition-all duration-200 hover-color-dark-gray:hover ${window.document.location.pathname === "/index.html" ? "text-black" : null }"
-              href="index.html"
-              >Startsida</a
-            >
-          </li>
+          <li class="transition-all duration-200 hover-color-dark-gray:hover ${window.document.location.pathname === "/kontakt.html" ? "text-black" : null }">
+            <a href="kontakt.html">Kontakt</a></li>
+          <li class="transition-all duration-200 hover-color-dark-gray:hover ${window.document.location.pathname === "/produkter.html" ? "text-black" : null }">
+            <a href="produkter.html" >Produkter</a></li>
+          <li class="transition-all duration-200 hover-color-dark-gray:hover ${window.document.location.pathname === "/order.html" ? "text-black" : null }">
+            <a href="order.html">Beställning</a></li>
+          <li class="transition-all duration-200 hover-color-dark-gray:hover ${window.document.location.pathname === "/index.html" ? "text-black" : null }">
+            <a href="index.html" >Startsida</a></li>
         </ul>
       </nav>
     `
