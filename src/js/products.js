769edea9a405696b95c1bd3bbb89af508b498b78
ThunsOJ <oscar_jidaker@icloud.com>
2023-03-17 17:26:35 +0100

    En väldigt slarvig snabbfix för produktbilder.

--- src/js/products.js
+++ src/js/products.js
@@ -7,10 +7,16 @@ function getAllProducts(){
           data.forEach((product) => {
             const productElement = document.createElement("div");
             productElement.innerHTML = `
-			  <h3>${product.title}</h3>
-			  <img src="${product.image}" alt="${product.title}">
-			  <p>${product.description}</p>
-			  <p>${product.price}</p>
+			  
+        <figure class="img-header">
+			    <img src="${product.image}" alt="${product.title}">
+        </figure>
+        
+        <div class="product-body">
+          <h3>${product.title}</h3>
+			    <p>${product.description}</p>
+			    <p>${product.price}</p>
+        </div>
 			`;
             productsContainer.appendChild(productElement);
           });
