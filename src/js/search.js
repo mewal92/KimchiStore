4f9d15029dd5e2cacca9b7cf00a08faa25de5a6e
Lamers <gustav@henrikssons.org>
2023-03-17 22:25:20 +0100

    Dynamic search bar

---
+++ src/js/search.js
@@ -0,0 +1,32 @@
+import list from "./fetchcount.js";
+//let testList = ["brun", "blå", "gul"];
+
+const searchResult = document.querySelectorAll(".searchResult");
+const search = document.querySelector("#search");
+search.value = "";
+
+const searchDrop = document.querySelector(".searchDropdown");
+
+search.addEventListener('keyup', (e) =>{
+    if(search.value == "" || search.value == null){
+        searchDrop.innerHTML = "";
+    } else {
+        searchDrop.innerHTML = "";
+        list.forEach((e) =>{
+        let word = e.toLowerCase();
+        let searchWord = search.value.toLowerCase();
+        if(word.includes(searchWord)){
+            let element = document.createElement("span");
+            element.classList.add("searchResult");
+            element.innerText = e;
+            searchDrop.appendChild(element);
+            element.addEventListener("click", (e) => {
+                search.value = e.target.innerText;
+                searchDrop.innerHTML = `
+                    <span class="searchResult">${search.value}</span>
+                `;
+                })
+            }
+        })
+    }
+});
