b3e186e60a736c46f78a2360d590a1725aa1623d
ThunsOJ <oscar_jidaker@icloud.com>
2023-03-20 16:36:40 +0100

    centrerat och stoppat ner pris texten

--- src/js/products.js
+++ src/js/products.js
@@ -7,15 +7,17 @@ function getAllProducts(){
           data.forEach((product) => {
             const productElement = document.createElement("div");
             productElement.innerHTML = `
-        <figure class="img-header">
-			    <img src="${product.image}" alt="${product.title}">
-        </figure>
+
+            <figure class="img-header">
+			        <img src="${product.image}" alt="${product.title}">
+            </figure>
         
-        <article class="product-body">
-          <h3>${product.title}</h3>
-			    <p class="price">${product.price}:-</p>
-        </article>
-			`;
+            <article class="product-body">
+              <h3>${product.title}</h3>
+			        <p class="price">${product.price}:-</p>
+            </article>
+            
+			    `;
             productsContainer.appendChild(productElement);
           });
         })
